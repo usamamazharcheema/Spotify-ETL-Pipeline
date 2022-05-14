a8af57cddb4eaf076ee1af1c5a4148a134004f21
Usama <usamamazharcheema@gmail.com>
2022-05-14 10:09:12 +0200

    Added data to database

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import datetime
 import sqlite3
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
-USER_ID = "add user id"
-Token = "add access token"
+USER_ID = "Add user id"
+Token = "Add access token"
 
 def check_if_valid_data(df: pd.DataFrame) -> bool:
 
@@ -28,6 +28,7 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
         raise Exception("Null values found")
 
     #check that all timestamps are of yesterday's date
+    '''
     yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
     yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
 
@@ -35,7 +36,7 @@ def check_if_valid_data(df: pd.DataFrame) -> bool:
     for timestamp in timestamps:
         if datetime.datetime.strptime(timestamp, "%Y-%m-%d") != yesterday:
             raise Exception("Atleast one of the returned songs doesn't come from within the last 24 hours")
-
+    '''
 if __name__ == '__main__':
     headers = {
         "Accept": "application/json",
@@ -44,11 +45,11 @@ if __name__ == '__main__':
     }
 
     today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=1)
+    yesterday = today - datetime.timedelta(days=5)
     yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
     r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
     data = r.json()
-    print(data)
+    #print(data)
 
     song_names = []
     artist_names = []
@@ -69,7 +70,31 @@ if __name__ == '__main__':
     }
 
     song_df = pd.DataFrame(song_dict, columns= ["song_name", "artist_name", "played_at", "timestamp"])
-
+    print(song_df)
     # Validate
     if check_if_valid_data(song_df):
         print("Data valid, proceed to load stage")
+
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect("my_played_tracks.sqlite")
+    cursor = conn.cursor()
+
+    sql_query = """ 
+    CREATE TABLE IF NOT EXISTS my_played_tracks (
+    song_name VARCHAR (200),
+    artist_name VARCHAR (200),
+    played_at VARCHAR (200),
+    timestamp VARCHAR (200),
+    CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+    cursor.execute(sql_query)
+    print("Opened database succesfully")
+
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists="append")
+    except:
+        print("Data alreay exists in the database")
+
+    conn.close()
+    print("close database")
